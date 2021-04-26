5ccc4a7923c18b6a93387365995f07e65dd6a3c3
GerganaLp <77080812+GerganaLp@users.noreply.github.com>
2021-04-26 16:53:56 +0300

    Add files via upload

---
+++ Counter-Strike/include/Defines.h
@@ -0,0 +1,26 @@
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+typedef enum {
+  GLOCK, DESERT_EAGLE
+} PistolType;
+
+enum GlockDefines {
+  ROUNDS_PER_FIRE = 3
+};
+
+enum PlayerDefines {
+  PLAYER_ONE, PLAYER_TWO, PLAYERS_COUNT
+};
+
+typedef struct {
+  int health;
+  int armor;
+} PlayerVitalData;
+
+/*Increase in case you want to play with more players in team*/
+typedef enum {
+  PLAYERS_IN_TEAM = 1
+} TeamDefines;
+
+#endif /* DEFINES_H_ */
