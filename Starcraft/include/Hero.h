5bf9adbcf5b13e9df3aba99302bf577a5b28ccf9
GerganaLp <77080812+GerganaLp@users.noreply.github.com>
2021-04-26 16:42:04 +0300

    Add files via upload

---
+++ Starcraft/include/Hero.h
@@ -0,0 +1,35 @@
+#ifndef HERO_H_
+#define HERO_H_
+
+#include "Defines.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_SPELL_NAME_SIZE 20
+#define MAX_HERO_NAME_SIZE 20
+#define MAX_SPELLS 2
+#define MAX_CHAMPIONS 3
+
+typedef struct {
+  char name[MAX_SPELL_NAME_SIZE];
+  int manaCost;
+} Spell;
+
+typedef struct {
+  Spell spells[MAX_SPELLS];
+  char name[MAX_HERO_NAME_SIZE];
+  int maxMana;
+  int currMana;
+  int manaRegenRate;
+} Hero;
+
+void heroBaseInit(Hero *hero, const char *inputName, int inputMaxMana,
+                  int inputManaRegenRate);
+
+void heroSpellsInit(Hero *hero, const char *basicSpellName,
+                    int basicSpellManaCost, const char *ultimateSpellName,
+                    int ultimateSpellManaCost);
+
+#endif /* HERO_H_ */
